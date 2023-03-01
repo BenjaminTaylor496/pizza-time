23b05e05d767390a5d771f51a45f14f06751bb6d
BenjaminTaylor496 <chechnya386@gmail.com>
2023-03-01 22:23:57 +0500

    created new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { Header } from './components/Header';
+import { Categories } from './components/Categories';
+import { Sorting } from './components/Sorting';
+import { PizzaBlock } from './components/PizzaBlock';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div class='wrapper'>
+			<Header />
+			<div class='content'>
+				<div class='container'>
+					<div class='content__top'>
+						<Categories />
+						<Sorting />
+					</div>
+					<h2 class='content__title'>Всё меню</h2>
+					<div class='content__items'>
+						<PizzaBlock title='Пицца четыре сыра' price={400} />
+						<PizzaBlock title='Домашняя пицца' price={999} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
+
+//rfc - быстро создает шаблон компонента
 
 export default App;
---
+++ src/components/Categories.jsx
@@ -0,0 +1,14 @@
+export const Categories = () => {
+	return (
+		<div class='categories'>
+			<ul>
+				<li class='active'>Все</li>
+				<li>Мясное</li>
+				<li>Вегетарианское</li>
+				<li>Гриль</li>
+				<li>Острые</li>
+				<li>Закрытые</li>
+			</ul>
+		</div>
+	);
+};
