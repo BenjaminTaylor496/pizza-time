e305841108500373d3d4421de678fc674350838f
BenjaminTaylor496 <chechnya386@gmail.com>
2023-05-16 01:31:37 +0500

    create page where showed pizza info

---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router';
+
+const FullPizza = () => {
+	const [pizza, setPizza] = useState();
+	const { id } = useParams();
+
+	useEffect(() => {
+		async function fetchPizza() {
+			try {
+				const { data } = await axios.get(
+					'https://642e83dd8ca0fe3352d1a166.mockapi.io/pizzas/' + id,
+				);
+				setPizza(data);
+			} catch (error) {
+				alert('Error: ' + error);
+			}
+		}
+
+		fetchPizza();
+	}, []);
+
+	if (!pizza) {
+		return 'Loading...';
+	} //До того как вытаскивать что-то из undefined, сначала проводится проверка: "нужно ли это делать"
+
+	// console.log(params);
+
+	return (
+		<div className='container'>
+			<img src={pizza.imageUrl} alt='img' />
+			<h2>{pizza.title}</h2>
+			<h3>{pizza.price} ₽</h3>
+		</div>
+	);
+};
+
+export default FullPizza;
